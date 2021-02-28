0f7ac4520a27f6acb40e57c7e24f59e9d870db65
PranavSandeep <62299702+PranavSandeep@users.noreply.github.com>
2021-02-28 22:54:09 +0530

    Delete Test.py

--- Test.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-URL = 'https://en.wikipedia.org/wiki/List_of_game_engines'
-content = requests.get(URL)
-
-soup = BeautifulSoup(content.text, 'html.parser')
-for link in soup.find_all('a'):
-    print(link.get('href'))
